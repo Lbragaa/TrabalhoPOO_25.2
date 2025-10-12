949f2a3a4806ce683a05aa0e45ec4ec9e7968ac2
Brunotardin06 <btardinfernandes@gmail.com>
2025-10-12 18:02:35 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -10,7 +10,7 @@ class Tabuleiro {
     // Número padrão de casas no Banco Imobiliário
     private static final int NUM_CASAS = 40;
     // Posição da prisão (geralmente casa 10)
-    private static final int POSICAO_VAI_PRA_PRISAO = 26
+    private static final int POSICAO_VAI_PRA_PRISAO = 26;
     private static final int POSICAO_PRISAO = 10;
 
     // Lista de propriedades existentes no tabuleiro
@@ -181,6 +181,7 @@ class Tabuleiro {
     baralhoSorteReves.offer(new Carta(TipoCarta.SAIDA_LIVRE, 0));
     }
 }
+
 
 
 
