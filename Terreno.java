aa09cf2c7887025e2efc5bf9f2c24b8f37def8f1
LBraga28 <luancarlosalm@gmail.com>
2025-10-12 01:34:00 -0300

    Update no terreno, agora extend de propriedade

--- Terreno.java
+++ Terreno.java
@@ -1,68 +1,44 @@
 package model;
 
-public class Terreno {
+public class Terreno extends Propriedade {
 
-    private String nome;
-    private Jogador proprietario;
-    private int preco;
-    private int valorCasa;
     private int numCasas;
-    private int aluguelBase;
 
     public Terreno(String nome, int preco, int valorCasa, int aluguelBase) {
-        this.nome = nome;
-        this.preco = preco;
+        // Chama o construtor da classe-mãe Propriedade
+        super(nome, preco, aluguelBase);
         this.valorCasa = valorCasa;
-        this.aluguelBase = aluguelBase;
         this.numCasas = 0;
-        this.proprietario = null;
-    }
-
-    // --------- PROPRIEDADE ---------
-    public Jogador getProprietario() {
-        return proprietario;
-    }
-
-    public void setProprietario(Jogador proprietario) {
-        this.proprietario = proprietario;
-    }
-
-    public boolean estaDisponivel() {
-        return proprietario == null;
     }
 
     // --------- CONSTRUÇÃO ---------
+    @Override
     public boolean podeConstruir() {
         // Pode construir até 4 casas (sem hotel nesta iteração)
         return numCasas < 4;
     }
 
+    @Override
     public void adicionaCasa() {
         if (podeConstruir()) {
             numCasas++;
         }
     }
 
+    @Override
     public int getNumCasas() {
         return numCasas;
     }
 
     // --------- ALUGUEL ---------
+    @Override
     public int calculaAluguel() {
-        // Regra simples: aluguel base * número de casas (mínimo 1)
+        // Aluguel base vezes número de casas (mínimo 1)
         if (numCasas == 0) return aluguelBase;
         return aluguelBase * numCasas;
     }
 
     // --------- GETTERS ---------
-    public String getNome() {
-        return nome;
-    }
-
-    public int getPreco() {
-        return preco;
-    }
-
     public int getValorCasa() {
         return valorCasa;
     }
